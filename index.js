4a70ba2dd3b0c30b793b6314aa5f6e0b58a99bf6
Kritagya Singh Chouhan <kritagya2022@gmail.com>
2024-07-03 00:43:55 +0530

    Implemented display of basic text on the terminal using the command.

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import figlet from 'figlet';
+
+console.log(chalk.green(figlet.textSync('Welcome!', { horizontalLayout: 'full' })));
+console.log(chalk.blue('My name is Kritagya Singh Chouhan.'));
+console.log(chalk.yellow('I am a web developer specializing in JavaScript and other technologies.'));
+console.log(chalk.magenta('Feel free to contact me at your.email@example.com.'));
